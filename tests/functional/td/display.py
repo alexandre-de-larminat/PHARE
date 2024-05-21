a1862adfde74b6182455834695e3c98ba2ec5053
Hiro Protagonist <a.delarminat01@gmail.com>
2024-05-21 17:46:12 -0400

    delete printfs

--- tests/functional/td/display.py
+++ tests/functional/td/display.py
@@ -17,20 +17,15 @@ time = get_times_from_h5(os.path.join(run_path, "EM_B.h5"))
 
 plot_time = time[int(len(time)-1)]
 first_time = time[1]
+half_time = time[int(len(time)/2)]
 
 
 B = run.GetB(plot_time)
 by, xby = flat_finest_field(B, "By")
 
-E = run.GetE(plot_time)
-Ex, xEx = flat_finest_field(E, "Ex")
-Ey, xEy = flat_finest_field(E, "Ey")
-Ez, xEz = flat_finest_field(E, "Ez")
+B1 = run.GetB(half_time)
+by1, xby1 = flat_finest_field(B1, "By")
 
-E0 = run.GetE(first_time)
-Ex0, xEx0 = flat_finest_field(E0, "Ex")
-Ey0, xEy0 = flat_finest_field(E0, "Ey")
-Ez0, xEz0 = flat_finest_field(E0, "Ez")
 
 velocity_ions = run.GetVi(plot_time)
 velocity_electrons = run.GetVe(plot_time)
@@ -73,6 +68,7 @@ def BY(x):
 
 ax0, ax1, ax2, ax3, ax4, ax5 = axarr
 
+ax0.plot(xby1, by1, color="r", ls="--")
 ax0.plot(xby, by, color="k", ls="-")
 ax0.plot(xby, BY(xby), color="darkorange", ls="--")
 
@@ -98,14 +94,32 @@ ax5.plot(xFex, Fex, color="r", ls="--")
 
 fig.savefig("td1d_pic_info.png")
 
+E = run.GetE(plot_time)
+Ex, xEx = flat_finest_field(E, "Ex")
+Ey, xEy = flat_finest_field(E, "Ey")
+Ez, xEz = flat_finest_field(E, "Ez")
+
+E0 = run.GetE(first_time)
+Ex0, xEx0 = flat_finest_field(E0, "Ex")
+Ey0, xEy0 = flat_finest_field(E0, "Ey")
+Ez0, xEz0 = flat_finest_field(E0, "Ez")
+
+E1 = run.GetE(half_time)
+Ex1, xEx1 = flat_finest_field(E1, "Ex")
+Ey1, xEy1 = flat_finest_field(E1, "Ey")
+Ez1, xEz1 = flat_finest_field(E1, "Ez")
+
 fig, axarr = plt.subplots(nrows=3, figsize=(8, 10))
 ax0, ax1, ax2 = axarr
 
 ax0.plot(xEx, Ex, color="k", ls="-")
 ax0.plot(xEx0, Ex0, color="r", ls="--")
+ax0.plot(xEx1, Ex1, color="b", ls="--")
 ax1.plot(xEy, Ey, color="k", ls="-")
 ax1.plot(xEy0, Ey0, color="r", ls="--")
+ax1.plot(xEy1, Ey1, color="b", ls="--")
 ax2.plot(xEz, Ez, color="k", ls="-")
 ax2.plot(xEz0, Ez0, color="r", ls="--")
+ax2.plot(xEz1, Ez1, color="b", ls="--")
 
 fig.savefig("td1d_pic_E.png")
